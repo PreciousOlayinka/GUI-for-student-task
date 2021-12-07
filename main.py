fd57d0c7df1be75e6e212222d4babc0971dd5149
16polayinka <>
2021-12-07 11:40:00 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import tkinter as tk 
+window = tk.Tk()
+window.geometry("500x500")
+lable = tk.Lable(text="Name")
+entry = tk.Entry()
+label.pack()
+entry.pack()
+name = entry.get()
+name
+entry.delete(0)
+
+window.mainloop()
+
+
